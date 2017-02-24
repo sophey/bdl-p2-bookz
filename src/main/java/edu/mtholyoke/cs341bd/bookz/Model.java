d2572940d5aa21a600c89d8f4f3487fc5556aeeb
Sophey <sophey.dong@gmail.com>
2017-02-24 14:15:20 -0500

    put books starting with characters into a hashmap

--- src/main/java/edu/mtholyoke/cs341bd/bookz/Model.java
+++ src/main/java/edu/mtholyoke/cs341bd/bookz/Model.java
@@ -7,33 +7,42 @@ import java.util.List;
 import java.util.Map;
 
 public class Model {
-	Map<String, GutenbergBook> library;
+  Map<String, GutenbergBook> library;
+  Map<Character, List<GutenbergBook>> booksStartingWith;
 
-	public Model() throws IOException {
-		// start with an empty hash-map; tell it it's going to be big in advance:
-		library = new HashMap<>(40000);
-		// do the hard work:
-		DataImport.loadJSONBooks(library);
-	}
+  public Model() throws IOException {
+    // start with an empty hash-map; tell it it's going to be big in advance:
+    library = new HashMap<>(40000);
+    // do the hard work:
+    DataImport.loadJSONBooks(library);
+    // store books starting with different characters in map
+    storeBooksStartingWith();
+  }
 
-	public GutenbergBook getBook(String id) {
-		return library.get(id);
-	}
+  /**
+   * Add the books starting with each character to the HashMap for easy
+   * retrieval.
+   */
+  public void storeBooksStartingWith() {
+    booksStartingWith = new HashMap<>();
+    for (GutenbergBook book : library.values()) {
+      char first = Character.toUpperCase(book.title.charAt(0));
+      if (!booksStartingWith.containsKey(first)) {
+        booksStartingWith.put(first, new ArrayList<>(10000));
+      }
+      booksStartingWith.get(first).add(book);
+    }
+  }
 
-	public List<GutenbergBook> getBooksStartingWith(char firstChar) {
-		// TODO, maybe it makes sense to not compute these every time.
-		char query = Character.toUpperCase(firstChar);
-		List<GutenbergBook> matches = new ArrayList<>(10000); // big
-		for (GutenbergBook book : library.values()) {
-			char first = Character.toUpperCase(book.title.charAt(0));
-			if(first == query) {
-				matches.add(book);
-			}
-		}
-		return matches;
-	}
+  public GutenbergBook getBook(String id) {
+    return library.get(id);
+  }
 
-	public List<GutenbergBook> getRandomBooks(int count) {
-		return ReservoirSampler.take(count, library.values());
-	}
+  public List<GutenbergBook> getBooksStartingWith(char firstChar) {
+    return booksStartingWith.get(firstChar);
+  }
+
+  public List<GutenbergBook> getRandomBooks(int count) {
+    return ReservoirSampler.take(count, library.values());
+  }
 }
